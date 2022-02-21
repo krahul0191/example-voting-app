a49bf372d7afdb8aa6745b6e10e0b82e8c0ce840
krahul0191 <99867275+krahul0191@users.noreply.github.com>
2022-02-21 12:59:57 +0530

    Update kubernetes.tf

--- Infra_provision/kubernetes.tf
+++ Infra_provision/kubernetes.tf
@@ -1,21 +1,5 @@
 # We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.97.0"
-    }
-  }
-}
-provider "azurerm" {
-  features {}
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
- }
-
 resource "azurerm_resource_group" "resource_group" {
   name     = var.rg_name
   location = var.location
